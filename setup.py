ad5ea07dff6ea9de9603be648f51d702a8b56747
ggallard <ggallard@identicum.com>
2021-09-06 01:29:46 -0300

    fixed dependencies

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Round Services',
     author_email='ggallard@roundservices.biz',
     license='MIT License',
-    install_requires=['requests', 'base64'],
+    install_requires=['requests'],
     packages=['rs.midpoint'],
     zip_safe=False,
     python_requires='>=3.0'
