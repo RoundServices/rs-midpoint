9da8f00f07db7087b4daa53601fa0ea4903bdbc9
ggallard <ggallard@identicum.com>
2021-09-06 01:20:14 -0300

    initial version, split from rs-partners

---
+++ README.md
@@ -0,0 +1,10 @@
+# rs-midpoint
+
+## Pre-requisites
+- Python > 3.x
+- [rs-utils](https://github.com/RoundServices/rs-utils)
+
+## Deploy
+```sh
+python3 -m pip install --upgrade git+https://github.com/RoundServices/rs-midpoint.git@main
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+# rs-midpoint is available under the MIT License. https://github.com/roundservices/rs-midpoint/
+# Copyright (c) 2021, Round Services LLC - https://roundservices.biz/
+#
+# Author: Gustavo J Gallardo - ggallard@roundservices.biz
+#
+
+from setuptools import setup
+
+setup(
+    name='rs-midpoint',
+    version='1.0.0',
+    description='Python utilities for Midpoint',
+    url='git@github.com:RoundServices/rs-midpoint.git',
+    author='Round Services',
+    author_email='ggallard@roundservices.biz',
+    license='MIT License',
+    install_requires=['requests', 'base64'],
+    packages=['rs.midpoint'],
+    zip_safe=False,
+    python_requires='>=3.0'
+)
