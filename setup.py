3cf85939c5aeb671e935e8ef5f7f33d35b3a839c
Gustavo J Gallardo <ggallard@identicum.com>
2021-11-18 16:09:09 -0300

    remove unpublished dependency

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Round Services',
     author_email='ggallard@roundservices.biz',
     license='MIT License',
-    install_requires=['requests', 'rs.utils'],
+    install_requires=['requests'],
     packages=['rs.midpoint'],
     zip_safe=False,
     python_requires='>=3.0'
